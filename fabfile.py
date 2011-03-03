bee78093a7af7cd0d81f017bfb0bcf1caaba5514
Ivan Willig <iwillig@gmail.com>
2011-03-03 08:34:53 +0300

    Fab file works, we can update the gateway with a single command.

---
+++ fabfile.py
@@ -0,0 +1,11 @@
+from fabric.api import env, run, cd, sudo
+
+location = '/usr/local/gateway-env/gateway'
+
+env.hosts.extend(['178.79.140.99'])
+
+def update_gateway():
+    with cd(location):
+        run('git pull origin master')
+        run('../bin/python setup.py install')
+        sudo('/etc/init.d/apache2 reload')
