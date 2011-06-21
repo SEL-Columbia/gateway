80706f65d903cf5478c476fdd5b87573729047af
Ivan Willig <iwillig@gmail.com>
2011-06-21 10:42:34 -0400

    Working on making the tests actually test something useful.

--- gateway/tests.py
+++ gateway/tests.py
@@ -1,147 +1,58 @@
 import unittest
-from sqlalchemy.orm.query import Query
-from webob.exc import HTTPFound
+import uuid
+import urllib2
+import urllib
 from pyramid import testing
 
 
-class GatewayTests(unittest.TestCase):
+def make_request(msg, interface, phone="+22365489009"):
+    data = urllib.urlencode({'message': msg,
+                             'number': phone,
+                             'uuid': str(uuid.uuid4()) })
+    return urllib2.Request(
+        data=data, url='http://localhost:6543/interface/send/%s' % interface.id)
+
+
+class FunctionalTests(unittest.TestCase):
     """ Unit tests for the Gateway UI """
     def setUp(self):
         self.config = testing.setUp()
+        import ConfigParser
         from gateway.models import DBSession
         from gateway.models import initialize_sql
-        initialize_sql('sqlite://')
+        config = ConfigParser.ConfigParser()
+        config.readfp(open('testing.ini'))
+        db_string = config.get('app:gateway', 'db_string')
+        initialize_sql(db_string)
+        from gateway import main
+        from webtest import TestApp
+        app = main(None, **{'db_string': db_string,
+                            'mako.directories': config.get('app:gateway', 'mako.directories')})
+        self.testapp = TestApp(app)
         self.session = DBSession()
 
     def tearDown(self):
         testing.tearDown()
 
-    def testAddKannelInterface(self):
-        from gateway.models import KannelInterface
-        interface = KannelInterface(name='Kannel',
-                                    location='USA',
-                                    host='localhost',
-                                    port='5432',
-                                    provider='Ivan',
-                                    username='kannel',
-                                    password='kannel')
-        self.session.add(interface)
-        self.session.flush()
-        query = self.session.query(KannelInterface)
-        self.assertEqual(query.count(), 1)
-
-    def testAddNetbookInterface(self):
-        from gateway.models import NetbookInterface
-        interface = NetbookInterface(name='Netbook',
-                                     provider='Ivan',
-                                     location='USA')
-        self.session.add(interface)
-        self.session.flush()
-        query = self.session.query(NetbookInterface)
-        self.assertEqual(query.count(), 1)
-
-    def testAddingMeters(self):
+    def test_number_of_meter(self):
         from gateway.models import Meter
-        meter = Meter(
-            name='Test meter',
-            phone='181812345',
-            location='USA',
-            battery=100,
-            status=1,
-            panel_capacity=100)
-        self.session.add(meter)
-        self.session.flush()
-        query = self.session.query(Meter)
-        self.assertEqual(query.count(), 1)
+        meters = self.session.query(Meter).all()
+        self.assertEquals(len(meters), 1)
 
-    def testAddingAccount(self):
-        from gateway.models import Account
-        a = Account(name='default',
-                    phone='19291212',
-                    lang='fr')
-        self.session.add(a)
-        self.session.flush()
-        q = self.session.query(Account)
-        self.assertEqual(q.count(), 1)
+    def test_index(self):
+        res = self.testapp.get('/', status=200)
+        self.assertEquals(res.status, '200 ok')
 
-    def testAddingCircuits(self):
+    def test_number_of_circuits(self):
         from gateway.models import Circuit
-        c = Circuit(meter=None,
-                    account=None,
-                    energy_max=100,
-                    power_max=100,
-                    ip_address='192.168.1.200',
-                    status=1,
-                    credit=1)
-        self.session.add(c)
-        self.session.flush()
-        q = self.session.query(Circuit)
-        self.assertEquals(q.count(), 1)
+        circuits = self.session.query(Circuit).all()
+        self.assertEquals(len(circuits), 11)
 
-    def testAddingAnIncomingMessage(self):
-        from gateway.models import IncomingMessage
-        import uuid
-        msg = IncomingMessage(number='181812345',
-                              text='Hello world',
-                              uuid=str(uuid.uuid4()),
-                              communication_interface=None)
-        self.session.add(msg)
-        self.session.flush()
-        q = self.session.query(IncomingMessage)
-        self.assertEquals(q.count(), 1)
+    def test_number_of_comm_interfaces(self):
+        from gateway.models import CommunicationInterface
+        interfaces = self.session.query(CommunicationInterface).all()
+        self.assertEquals(len(interfaces), 1)
 
-    def testDashboardIndex(self):
-        request = testing.DummyRequest()
-        from gateway.handlers import Dashboard
-        from formalchemy.tables import Grid
-        handler = Dashboard(request)
-        # test index
-        index = handler.index()
-        self.assertEqual(type(index['logs']), Grid)
-        self.assertEqual(type(index['breadcrumbs']), list)
-
-    def testManage(self):
-        request = testing.DummyRequest()
-        from gateway.handlers import ManageHandler
-        handler = ManageHandler(request)
-        index = handler.index()
-        self.assertEqual(type(index['breadcrumbs']), list)
-
-    def testAlerts(self):
-        from gateway.handlers import AlertHandler
-        handler = AlertHandler(testing.DummyRequest())
-        make = handler.make()
-        self.assertEqual(type(make['breadcrumbs']),list)
-        self.assertEqual(type(make['interfaces']),list)
-
-## Below need to be functional tests.
-
-    # def testAlertsSend(self):
-    #     from gateway.handlers import AlertHandler
-    #     request = testing.DummyRequest(params={'interface' : 1,
-    #                                            'text'      : 'This is an test',
-    #                                            'number'    : '181812345'})
-    #     handler = AlertHandler(request)        
-    #     send = handler.send_test()
-
-    def testAddModels(self):
-        # test adding a Meter
-        # totally broken from within the testing
-        # request. FIXME
-        from gateway.handlers import AddClass
-
-    def testUser(self):
-        request = testing.DummyRequest()
-        from gateway.handlers import UserHandler
-        handler = UserHandler(request)
-
-        # test login
-        login = handler.login()
-        self.assertEqual(login['url'], 'http://example.com/login',)
-        self.assertEqual(login['login'], '')
-        self.assertEqual(login['password'], '')
-        self.assertEqual(login['came_from'], '/')
-
-        logout = handler.logout()
-        self.assertEqual(type(logout), HTTPFound)
-        self.assertEqual(logout.status, '302 Found')
+    def test_balance_english(self):
+        from gateway.models import CommunicationInterface
+        from gateway.models import Circuit
